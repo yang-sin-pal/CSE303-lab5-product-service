fe06a3ef9a32df586c0de02b47ef01d275fdf3db
Duong Tan Phat <phat.duongtan.cit22@eiu.edu.vn>
2026-02-05 10:42:05 +0700

    feat: complete function and test url link when moving submodule to new personal repo

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.get('/:id', async (req, res) => {
     console.error('Get product error:', error);
     res.status(500).json({error: 'Internal server error'});
   }
+  
 });
 
 // Create product
